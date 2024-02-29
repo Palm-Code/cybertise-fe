652751912898140f15993471dc323d1eadb86d48
rizkyalfiansyah <rizkkyaf@gmail.com>
2024-02-29 14:43:02 +0800

    feat: add otp form

--- core/ui/icons/index.ts
+++ core/ui/icons/index.ts
@@ -2,6 +2,7 @@ export { default } from "./moon/Moon.icon";
 export { default as MoonBackground } from "./moon/MoonBackground.icon";
 export { default as Cloud } from "./sun/Cloud.icon";
 export { default as Locker } from "./locker/Locker.icon";
+export { default as Locker2 } from "./locker/Locker2.icon";
 export { default as Hacker } from "./hacker/Hacker.icon";
 export { default as Company } from "./company/Company.icon";
 export { default as Logo } from "./logo/Logo.icon";
--- feature/auth/components/index.ts
+++ feature/auth/components/index.ts
@@ -1,3 +1,4 @@
 export { default as SignIn } from "./sign-in/SignIn.component";
 export { default as SuccessState } from "./success-state/SuccesState.component";
 export { default as SignUpType } from "./sign-up/SignUpType.component";
+export { default as MultiFactor } from "./multi-factor-auth/MultiFactor.component";
