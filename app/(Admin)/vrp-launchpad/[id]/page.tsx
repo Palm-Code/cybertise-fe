8a1b53924fc9dc18e597f3e6eb3bb0ee7e7b95a7
rizkyalfiansyah <rizkkyaf@gmail.com>
2024-03-27 16:44:27 +0800

    feat: asjust vrp launchpad for compane and mediator

--- app/(Admin)/vrp-launchpad/[id]/page.tsx
+++ app/(Admin)/vrp-launchpad/[id]/page.tsx
@@ -1,5 +1,13 @@
 import { CreateVrpFragment } from "@/feature/mediator/fragments";
+import { CreateVrpFragment as CreateCompanyVrpFragment } from "@/feature/company/fragments";
+import { getSession } from "@/service/server/session";
 
-export default function VRPLaunchpadDetailPage() {
+export default async function VRPLaunchpadDetailPage() {
+  const session = await getSession();
+
+  if (session?.user.role === "company") {
+    return <CreateCompanyVrpFragment />;
+  }
+
   return <CreateVrpFragment />;
 }
