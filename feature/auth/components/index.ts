944f3b9eca350df0cc4afe838692f6e3364c7641
Rizky Alfiansyah <37681694+RizkyAlfiansyah@users.noreply.github.com>
2024-05-20 17:01:02 +0800

    Revert "todev - add forgot password ui"

--- feature/auth/components/index.ts
+++ feature/auth/components/index.ts
@@ -2,4 +2,3 @@ export { default as SignIn } from "./sign-in/SignIn.component";
 export { default as SuccessState } from "./success-state/SuccesState.component";
 export { default as SignUpType } from "./sign-up/SignUpType.component";
 export { default as MultiFactor } from "./multi-factor-auth/MultiFactor.component";
-export { default as ForgotPassword } from "./forgot-password/ForgotPassword.component";
