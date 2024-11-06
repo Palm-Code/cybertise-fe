5d11a3403d5b3efa8ff60ba319192a62694c8dcf
RizkyAlfiansyah <rizkkyaf@gmail.com>
2024-11-06 11:46:12 +0800

    fix: error on build

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,22 +6,13 @@ import type { Metadata } from "next";
 import { cn } from "@/core/lib/utils";
 import { Role } from "@/types/admin/sidebar";
 import { getSession } from "@/service/server/session";
-import {
-  getQueryClient,
-  Hydrate,
-  ReactQueryProvider,
-  ThemeProvider,
-} from "@/core/provider";
+import { ReactQueryProvider, ThemeProvider } from "@/core/provider";
 import { Toaster } from "@/core/ui/components";
 import { getLocale, getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
 import { fetchGetCountryList } from "@/core/services/common";
 import CountryListInitializer from "@/core/zustands/country-list/initializer";
-import { dehydrate } from "@tanstack/react-query";
-import { prefetchGetUserData } from "@/core/react-query/server";
-import { UserInitializer } from "@/core/zustands/user";
 import { headers } from "next/headers";
-import { fetchGetUserData } from "@/core/services/server";
 
 export const revalidate = 0;
 
